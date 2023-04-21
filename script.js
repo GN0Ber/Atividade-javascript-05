01266374d93129176426f45518293555f655eb3c
Guilherme <gnbernnardo@gmail.com>
2023-04-20 22:40:37 -0300

    tela de login

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//alert("ola")
+function validar(){
+    let usuario = document.getElementById("usuario").value
+    let senha = document.getElementById("senha").value
+
+    if(usuario == "" && senha == ""){
+        alert("Preencha todos os campos")
+    }else{
+        alert("Acesso Permitido")
+        window.open('menu.html')
+    }
+
+}
